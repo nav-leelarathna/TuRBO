50f1c8bb7fbdf25a07af1cee7c2e96f3b65b6167
NavChang <39101388+NavChang@users.noreply.github.com>
2023-03-05 23:56:54 +0000

    £

---
+++ experiments/experiment1.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../")
+from turbo import TurboM, Turbo1
+
+'''
+Things to change
+trust_regions
+batch_size 
+dimensionality of problem
+seed
+
+We want to evaluate whether using trust regions does truly give us a benefit compared to normal BO, and also in setting where total number of runs is equal, i.e. increasing batch size.
+
+Secondly, I hypothesize that the benefit gained by the bandits diminished quickly as the dimensionality of the problem increases, test by ablating over the dimensionality of the problem.
+
+Replicate Figure 7 - shows why single agent BO only explores and optimises within a highly local volume of the search space
+
+Replicate Figure 7 - showing that increasing batch size gives us a linear improvement, plotting reward vs steps
+
+problems - branin, levy
+'''
